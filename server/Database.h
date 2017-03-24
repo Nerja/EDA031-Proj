87ec06986c71457a1b00829215a45ca1d6f54978
Marcus Rodan <marcusrodan@gmail.com>
2017-03-24 13:01:50 +0100

    db interface

---
+++ server/Database.h
@@ -0,0 +1,16 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+#include <vector>
+#include "newsgroup.h"
+#include "article.h"
+#include <string>
+class Database {
+  virtual std::vector<NewsGroup> list_newsgroups() const = 0;
+  virtual boolean create_newsgroup(std::string name) = 0;
+  virtual boolean delete_newsgroup(int int) = 0;
+  virtual Article read_article(int group, int article) = 0;
+  virtual void create_article(int group, std::string title, std::string author, std::string text) = 0;
+  virtual boolean delete_article(int group, int article) = 0;
+  virtual std::vector<Article> list_articles(int group) = 0;
+};
+#endif
