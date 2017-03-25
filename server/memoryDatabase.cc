a7bd8de78f7872e6665137033d332f0308e237c5
gelm0 <gustav.ek@gmail.com>
2017-03-25 15:30:51 +0100

    Merging

--- server/memoryDatabase.cc
+++ server/memoryDatabase.cc
@@ -1,14 +1,22 @@
+#include <string>
+#include <functional>
+#include <vector>
+
 #include "memoryDatabase.h"
+#include "newsgroup.h"
+#include "article.h"
 
 std::vector<NewsGroup> MemoryDatabase::list_newsgroups() const {
 
 }
 
 boolean MemoryDatabase::create_newsgroup(std::string name) {
-
+	hash<std::string> hash_id(name);
+	Newsgroup n(name, hash_id);
+		
 }
 
-boolean delete_newsgroup(int int) {
+boolean delete_newsgroup(int id) {
 
 }
 
