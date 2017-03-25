82fe677dd4392d2c333c35e2ed0b393820a0c97d
kimFra94 <friidrott1994@gmail.com>
2017-03-25 16:09:12 +0100

    database.h uppdaterad

--- server/database.h
+++ server/database.h
@@ -6,11 +6,11 @@
 #include <string>
 class Database {
 	virtual std::vector<NewsGroup> list_newsgroups() const = 0;
-	virtual boolean create_newsgroup(std::string name) = 0;
-	virtual boolean delete_newsgroup(int int) = 0;
+	virtual bool create_newsgroup(std::string name) = 0;
+	virtual bool delete_newsgroup(int id) = 0;
 	virtual Article read_article(int group, int article) = 0;
-	virtual void create_article(int group, std::string title, std::string author, std::string text) = 0;
-	virtual boolean delete_article(int group, int article) = 0;
-	virtual std::vector<Article> list_articles(int group) = 0; 
+	virtual bool create_article(int group, std::string title, std::string author, std::string text) = 0;
+	virtual bool delete_article(int group, int article) = 0;
+	virtual std::vector<Article> list_articles(int group) = 0;
 };
 #endif
