468524c55f06098e93ca2833c4e0f3786262a520
gelm0 <gustav.ek@gmail.com>
2017-03-26 19:46:16 +0200

    ÃÃ*ÃDelete and create

--- server/filedatabase.cc
+++ server/filedatabase.cc
@@ -3,10 +3,12 @@
 #include <functional>
 #include <utility>
 #include <map>
-#include <iostream>
 #include <sys/stat.h>
 #include <errno.h>
 #include <string.h>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
 
 #include "databaseExceptions.h"
 #include "article.h"
@@ -15,6 +17,7 @@
 
 
 const std::string PATH = "./newsgroups/";
+const std::string LIST_NEWSGROUP = "newsgrouplist";
 
 std::vector<NewsGroup> FileDatabase::list_newsgroups() const {}
 
@@ -22,21 +25,39 @@ void FileDatabase::create_newsgroup(std::string name) throw(invalid_group_name_e
 
 	std::hash<std::string> hash;
 	size_t hash_id = hash(name);
+	std::ofstream f;
+	f.open(PATH+LIST_NEWSGROUP);
 
 	std::string path = PATH;
 	path.append(name);
 	path.append("_"+std::to_string(hash_id));
 	const char* c = path.c_str();
-	std::cout << path << std::endl;
 	if( mkdir(c, 0777) == -1) {
 		throw invalid_group_name_exception();
 	}
+	f << name+"_"+std::to_string(hash_id) << "\n";
+	f.close();
 
 
 }
 
-void FileDatabase::delete_newsgroup(size_t id) throw(invalid_group_id_exception){
-	
+void FileDatabase::delete_newsgroup(int id) throw(invalid_group_id_exception){
+	std::ifstream f(PATH+LIST_NEWSGROUP, std::ifstream::in);
+	std::string line;
+	std::string string_id = std::to_string(id);
+	size_t p;
+
+	while( getline(f, line) ) {
+		p = line.find_last_of("_");
+			if(line.substr(p) == string_id ) {
+				std::string path = PATH;
+				path.append(line);
+				const char* c = path.c_str();
+				rmdir(c);
+				line.replace(line.find(line), line.length(), "");			
+			}
+	}
+		
 }
 
 Article FileDatabase::read_article(int group, int article)
